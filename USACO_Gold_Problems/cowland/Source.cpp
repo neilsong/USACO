5ffe7752eb8b322c2a8fdebe1fe64e74a21e9328
Laptop <laptop@laptop.com>
2019-11-08 14:57:26 -0600

    sln v1

---
+++ USACO_Gold_Problems/cowland/Source.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, Q;
+
+vector<pair<pair<int, int>, int>> pathchart;
+vector<pair<int, int>> includeind;
+vector<vector<int>> ancestor;
+
+int main(void) {
+	ifstream fin("cowland.in");
+	ofstream fout("cowland.out");
+	cin >> N >> Q;
+	for (int i = 0; i < N; i++) {
+
+	}
+}
