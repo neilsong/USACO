e5edb8d7ae21934fe8e10244dbfac8fb94203f18
desktop <desktop@desktop.com>
2019-11-29 14:03:54 -0600

    add lemonade silver and revegetate bronze. change gold out of sorts to work

--- gold/sort/Source.cpp
+++ gold/sort/Source.cpp
@@ -1,40 +1,95 @@
 #include <bits/stdc++.h>
 using namespace std;
+//fenwick tree from GeeksforGeeks
+const int MAX_VAL = 1000001;
 
-vector<int> arr;
-//set<int> sorted;
+/* Updates element at index 'i' of BIT. */
+void update(int i, int add, vector<int>& BIT)
+{
+	while (i > 0 && i < BIT.size())
+	{
+		BIT[i] += add;
+		i = i + (i & (-i));
+	}
+}
+
+/* Returns cumulative sum of all elements of
+   fenwick tree/BIT from start upto and
+   including element at index 'i'. */
+int sum(int copy, vector<int>& BIT)
+{
+	int i = copy;
+	int ans = 0;
+	while (i > 0)
+	{
+		ans += BIT[i];
+		i = i - (i & (-i));
+	}
+
+	return ans;
+}
+
+// Returns lower bound for k in BIT. 
+int findKthSmallest(int k, vector<int>& BIT)
+{
+	// Do binary search in BIT[] for given 
+	// value k. 
+	int l = 0;
+	int h = BIT.size();
+	while (l < h)
+	{
+		int mid = (l + h) / 2;
+		if (k <= sum(mid, BIT))
+			h = mid;
+		else
+			l = mid + 1;
+	}
+
+	return l;
+}
+
+// Insert x into BIT. We masically increment 
+// rank of all elements greater than x. 
+void insertElement(int x, vector<int>& BIT)
+{
+	update(x, 1, BIT);
+}
+
+// Delete x from BIT. We masically decreases 
+// rank of all elements greater than x. 
+void deleteElement(int x, vector<int>& BIT)
+{
+	update(x, -1, BIT);
+}
+
+// Returns rank of element. We basically 
+// return sum of elements from start to 
+// index x. 
+int findRank(int x, vector<int>& BIT)
+{
+	return sum(x, BIT);
+}
 
 int main(void) {
-	ifstream fin("sort.in");
-	ofstream fout("sort.out");
+	ifstream cin("sort.in");
+	ofstream cout("sort.out");
 
-	int n;
-	cin >> n;
-	arr.resize(n);
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
+	int n; cin >> n;
+	vector<int> arr(MAX_VAL);
+	vector<pair<int,int>> sorted(1);
+
+	int ans = 1;
+
+	for (int i = 1; i <= n; i++) {
+		int ins;
+		cin >> ins;
+		sorted.push_back({ ins, i });
 	}
-	bool sorted = 0;
-	int counter = 0;
-	while (!sorted) {
-		sorted = 1;
-		counter++;
-		for (int i = 0; i < n - 1; i++) {
-			if (arr[i] > arr[i + 1]) {
-				swap(arr[i], arr[i + 1]);
-			}
-		}
-		for (int i = 0; i < n - 1; i++) {
-			if (arr[i] > arr[i + 1]) {
-				swap(arr[i], arr[i + 1]);
-			}
-		}
-		for (int i = 0; i < n - 1; i++) {
-			if (arr[i] > arr[i + 1]) {
-				sorted = 0;
-			}
-		}
+	sort(sorted.begin(), sorted.end());
+	for (int i = 1; i <= n; i++) {
+		insertElement(sorted[i].second, arr);
+		int temp = i-findRank(i, arr);
+		ans = max(ans, temp);
 	}
-	cout << counter << endl;
-	return 0;
+	cout << ans<< endl;
 }
