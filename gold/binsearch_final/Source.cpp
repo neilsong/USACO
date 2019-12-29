f6fc70a0db1f36af89b1a22b8ea5f59049d3950f
Laptop <laptop@laptop.com>
2019-12-28 16:38:57 -0800

    readded minsegtree and added binsearch_final version

---
+++ gold/binsearch_final/Source.cpp
@@ -0,0 +1,12 @@
+//while (l < r)
+//
+//	int mid = (l + r + (1)) / 2;
+//
+//if (good)
+//--->r = mid
+//--->l = mid + 1
+//
+//---------------------------- -
+//
+//r = mid - 1;                     THIS WILL CAUSE 1 TO BE ADDED.
+//l = mid;
