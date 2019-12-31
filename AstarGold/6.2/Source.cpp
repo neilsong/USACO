15b51ab39645fd43ba4e0f88c708b04d5b04c5a9
Laptop <laptop@laptop.com>
2019-12-31 10:27:42 -0800

    checking out

--- AstarGold/6.2/Source.cpp
+++ AstarGold/6.2/Source.cpp
@@ -9,11 +9,19 @@ typedef vector<vector<ll>> vvll;
 typedef vector<pair<ll, ll>> vpair;
 typedef vector<vector<pair<ll, ll>>> vvpair;
 
+vvpair adj;
+ll n, m, k, q;
+vll dist;
 int main() {
-	ifstream fin(".in");
-	ofstream fout(".out");
-
-
-
+	cin >> n >> m >> k >> q;
+	for (int i = 0; i < n; i++) {
+		int a, b, w;
+		cin >> a >> b >> w; a--; b--;
+		adj[a].push_back({ b,w });
+	}
+	dist.resize(k);
+	for (int i = 0; i < k; i++) {
+		dijkstsra(i);
+	}
 	return 0;
 }
