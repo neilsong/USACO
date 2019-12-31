b19f79898bd0a9cbb09178dec0f65d75a6ac2931
Laptop <laptop@laptop.com>
2019-12-31 13:35:51 -0800

    day6

--- AstarGold/6.1/Source.cpp
+++ AstarGold/6.1/Source.cpp
@@ -1,18 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-#define INF 0x3f3f3f3f
-vector<ll> dx{ 1,-1,0,0 }, dy{ 0,0,-1,1 };
-typedef vector<ll> vll;
-typedef vector<vector<ll>> vvll;
-typedef vector<pair<ll, ll>> vpair;
-typedef vector<vector<pair<ll, ll>>> vvpair;
-
-int main() {
-	
+vector<vector<string>> mat;
+string orig = "";
 
 
 
-	return 0;
+int main(void) {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		char add;
+		cin >> add;
+		orig += add;
+	}
+	int i = 0, j = n - 1;
+	string final = "";
+	while (i < j) {
+		if (orig[i] < orig[j]) {
+			final += orig[i];
+			i++;
+		}
+		else if (orig[i] == orig[j]) {
+			int ti = i + 1, tj = j - 1;
+			while (ti < tj && orig[ti] == orig[tj]) {
+				ti++;
+				tj--;
+			}
+			if (orig[ti] < orig[tj]) {
+				final += orig[i];
+				i++;
+			}
+			else {
+				final += orig[j];
+				j--;
+			}
+		}
+		else {
+			final += orig[j];
+			j--;
+		}
+	}
+	final += orig[i];
+	for(int i= 0;i < final.length(); i++){
+		cout << final[i];
+		if ((i + 1) % 80 == 0)cout << endl;
+	}
 }
--- AstarGold/6.2/Source.cpp
+++ AstarGold/6.2/Source.cpp
@@ -8,20 +8,83 @@ typedef vector<ll> vll;
 typedef vector<vector<ll>> vvll;
 typedef vector<pair<ll, ll>> vpair;
 typedef vector<vector<pair<ll, ll>>> vvpair;
+vvpair adj, radj;
+unsigned long long n, m, k, q;
+vector<vector<unsigned long long>> dist;
+vector<vector<unsigned long long>> rdist;
 
-vvpair adj;
-ll n, m, k, q;
-vll dist;
+
+#define ullmax 18446744073709551615
+
+void dijkstra(int s) {
+	dist[s].assign(n, ullmax);
+	priority_queue<pair<unsigned long long, unsigned long long>, std::vector<pair<unsigned long long, unsigned long long>>, std::greater<pair<unsigned long long, unsigned long long>>> pq;
+	dist[s][s] = 0;
+	pq.push({ 0,s });
+	while (!pq.empty()) {
+		int v = pq.top().second; int vw = pq.top().first; pq.pop();
+		if (vw > dist[s][v]) continue;
+		for (auto u : adj[v]) {
+			if (dist[s][v] + u.second < dist[s][u.first]) {
+				dist[s][u.first] = dist[s][v] + u.second;
+				pq.push({ dist[s][u.first], u.first });
+			}
+		}
+	}
+}
+
+void rdijkstra(int s) {
+	rdist[s].assign(n, ullmax);
+	priority_queue<pair<unsigned long long, unsigned long long>, std::vector<pair<unsigned long long, unsigned long long>>, std::greater<pair<unsigned long long, unsigned long long>>> pq;
+	rdist[s][s] = 0;
+	pq.push({ 0,s });
+	while (!pq.empty()) {
+		int v = pq.top().second; int vw = pq.top().first; pq.pop();
+		if (vw > rdist[s][v]) continue;
+		for (auto u : radj[v]) {
+			if (rdist[s][v] + u.second < rdist[s][u.first]) {
+				rdist[s][u.first] = rdist[s][v] + u.second;
+				pq.push({ rdist[s][u.first], u.first });
+			}
+		}
+	}
+}
+
+
+
 int main() {
-	cin >> n >> m >> k >> q;
-	for (int i = 0; i < n; i++) {
-		int a, b, w;
-		cin >> a >> b >> w; a--; b--;
+	std::cin >> n >> m >> k >> q;
+	adj.resize(n); radj.resize(n);
+	for (int i = 0; i < n; ++i) {
+		unsigned long long a, b, w;
+		std::cin >> a >> b >> w; --a; --b;
 		adj[a].push_back({ b,w });
+		if (b > n - 1) continue;
+		radj[b].push_back({ a,w });
 	}
 	dist.resize(k);
-	for (int i = 0; i < k; i++) {
-		dijkstsra(i);
+	rdist.resize(k);
+	for (int i = 0; i < k; ++i) {
+		dijkstra(i);
+		rdijkstra(i);
 	}
+	unsigned long long num = 0, fans = 0;
+	for (int i = 0; i < q; i++) {
+		int a, b;
+		cin >> a >> b; a--; b--;
+		if (a > n - 1 || b > n - 1) continue;
+		unsigned long long tans = ullmax;
+		for (int j = 0; j < k; j++) {
+			unsigned long long sum = 0;
+			if (rdist[j][a] == ullmax || dist[j][b] == ullmax) continue;
+			sum = rdist[j][a] + dist[j][b];
+			tans = min(sum, tans);
+		}
+		if (tans == ullmax) continue;
+		++num;
+		fans += tans;
+	}
+	cout << num << endl << fans << endl;
 	return 0;
 }
+
